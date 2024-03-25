00ca38f16a5be50b982fbad32cc95055fded1dcf
Pierre Van Remoortere <pierrevanremoortere@Pierres-MacBook-Pro.local>
2024-03-25 22:08:46 +0100

    feat: appendentries rpc

--- raft/node.go
+++ raft/node.go
@@ -1,58 +1,171 @@
 package raft
 
+import (
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+)
+
 type Role int
+
 const (
 	Follower Role = iota
 	Candidate
 	Leader
 )
 
+func (r Role) String() string {
+	switch r {
+	case Follower:
+		return "Follower"
+	case Candidate:
+		return "Candidate"
+	case Leader:
+		return "Leader"
+	default:
+		return "Unknown"
+	}
+}
+
 type NodeState struct {
 	/* persistent state */
+	id          uint64
 	currentTerm uint64
-	votedFor uint64
-	logger Logger
+	votedFor    uint64
+	logger      Logger
 
 	/* volatile state */
 	commitIndex uint64
 	lastApplied uint64
 
 	/* volatile state for leaders */
-	nextIndex map[uint64]uint64
+	nextIndex  map[uint64]uint64
 	matchIndex map[uint64]uint64
+}
+
+func (n *NodeState) save() {
+	// todo: save to disk
+}
+
+type NodeConfig struct {
+	avgTimeout    int
+	timeoutWindow int
 }
 
 type Node struct {
 	state NodeState
-	role Role
+	role  Role
 
 	StateMachine StateMachine
-	RaftRPC RaftRPC
+	RaftRPC      RaftRPC
+	Peers        []Peer
+
+	electionChannel chan BallotResponse
+	mtx             sync.Mutex
+	timer           *time.Timer
 }
 
-func NewNode() *Node {
+type AppendEntriesRequest struct {
+	Term         uint64
+	LeaderId     uint64
+	PrevLogIndex uint64
+	PrevLogTerm  uint64
+	Entries      []LogEntry
+	LeaderCommit uint64
+}
+
+type AppendEntriesResponse struct {
+	Term    uint64
+	Success bool
+}
+
+func NewNode(id uint64) *Node {
 	return &Node{
 		state: NodeState{
+			id:          id,
 			currentTerm: 1,
-			votedFor: 0,
+			votedFor:    0,
 			commitIndex: 0,
 			lastApplied: 0,
-			nextIndex: make(map[uint64]uint64),
-			matchIndex: make(map[uint64]uint64),
-			logger: &InMemoryLogger{},
+			nextIndex:   make(map[uint64]uint64),
+			matchIndex:  make(map[uint64]uint64),
+			logger: &InMemoryLogger{
+				entries: []LogEntry{
+					{Term: 1, Index: 1, Command: []byte("init")},
+				},
+			},
 		},
-		role: Follower,
+		role:         Follower,
 		StateMachine: &DebugStateMachine{},
-		RaftRPC: &InMemoryRaftRPC{},
+		RaftRPC:      &InMemoryRaftRPC{},
+		timer:        time.NewTimer(time.Duration(200+rand.Intn(150)) * time.Millisecond),
 	}
 }
 
-
 func (n *Node) Start() {
 	/* Start the node */
+	go n.nodeDaemon()
+	
 }
 
 func (n *Node) AppendEntries(req AppendEntriesRequest) AppendEntriesResponse {
 	/* AppendEntries RPC */
-	return AppendEntriesResponse{}
+
+	if req.Term < n.state.currentTerm {
+		log.Println("AppendEntries: Request term is less than current term")
+		return AppendEntriesResponse{Term: n.state.currentTerm, Success: false}
+	}
+	lg, err := n.state.logger.Get(req.PrevLogIndex)
+	if err != nil || lg.Term != req.PrevLogTerm {
+		log.Println("AppendEntries: Log doesn't match")
+		return AppendEntriesResponse{Term: n.state.currentTerm, Success: false}
+	}
+
+	if len(req.Entries) == 0 {
+		// heatbeat
+		log.Println("AppendEntries: Heartbeat from leader")
+		n.RestartElectionTimer()
+		return AppendEntriesResponse{Term: n.state.currentTerm, Success: true}
+	}
+
+	logs, _ := n.state.logger.GetRange(req.PrevLogIndex + 1)
+	for i, entry := range logs {
+		// A node can't have a more up to date log than the leader
+		// So req.Entries[i] should always be valid / in bounds
+		if entry.Term != req.Entries[i].Term {
+			log.Println("AppendEntries: Log doesn't match, truncating from ", entry.Index, entry.Command)
+			n.state.logger.TruncateTo(entry.Index)
+			break
+		}
+	}
+	log.Println("AppendEntries: Appending new entries")
+	new := 0
+	for i, entry := range req.Entries {
+		_, err := n.state.logger.Get(entry.Index)
+		if err != nil {
+			new = i
+			break
+		}
+	}
+
+	n.state.logger.Append(req.Entries[new:])
+
+	return AppendEntriesResponse{n.state.currentTerm, true}
+}
+
+func (n *Node) RestartElectionTimer() {
+	/* Restart the election timer */
+	n.timer.Reset(time.Duration(200+rand.Intn(150)) * time.Millisecond)
+}
+
+func (n *Node) nodeDaemon() {
+	/* Timer daemon */
+	for {
+		select {
+		case <-n.timer.C:
+			// Election timeout
+			n.StartElection()
+		}
+	}
 }
---
+++ raft/node_test.go
@@ -0,0 +1,137 @@
+package raft
+
+import (
+	"testing"
+)
+
+func TestAppendEntriesSimple(t *testing.T) {
+	/*
+		A first election has been held and a leader is sending his first log
+	*/
+	node := NewNode(1)
+	logger := &InMemoryLogger{
+		entries: []LogEntry{
+			{Term: 1, Index: 1, Command: []byte("init")},
+		},
+	}
+	node.state.logger = logger
+
+	// The first data append entries request
+	// sent bn the leader to the followers
+	// The leader's term is 2 and he has not yet committed any data.
+	req1 := AppendEntriesRequest{
+		Term:         2,
+		LeaderId:     123,
+		PrevLogIndex: 1,
+		PrevLogTerm:  1,
+		Entries: []LogEntry{
+			{Term: 2, Index: 5, Command: []byte("a")},
+		},
+		LeaderCommit: 1,
+	}
+
+	resp := node.AppendEntries(req1)
+	if !resp.Success {
+		t.Errorf("Expected success")
+	}
+
+}
+
+func TestAppendEntriesWithConflict(t *testing.T) {
+	/*
+		A first election has been held and a leader is sending his first log
+	*/
+	node := NewNode(1)
+	logger := &InMemoryLogger{
+		entries: []LogEntry{
+			{Term: 1, Index: 1, Command: []byte("init")},
+			{Term: 2, Index: 2, Command: []byte("a")},
+		},
+	}
+	node.state.logger = logger
+
+	// The first data append entries request
+	// sent bn the leader to the followers
+	// The leader's term is 2 and he has not yet committed any data.
+	req1 := AppendEntriesRequest{
+		Term:         2,
+		LeaderId:     123,
+		PrevLogIndex: 1,
+		PrevLogTerm:  1,
+		Entries: []LogEntry{
+			{Term: 2, Index: 2, Command: []byte("a")},
+		},
+		LeaderCommit: 1,
+	}
+
+	resp := node.AppendEntries(req1)
+	if !resp.Success {
+		t.Errorf("Expected success")
+	}
+
+	log, _ := node.state.logger.Get(2)
+	if log.Term != 2 || log.Command[0] != 'a' {
+		t.Errorf("Wrong term %d", log.Term)
+	}
+
+	// The second data append entries request
+	// sent by the leader to the followers
+	// The leader's term is 2 and he has not yet committed any data.
+	req2 := AppendEntriesRequest{
+		Term:         2,
+		LeaderId:     123,
+		PrevLogIndex: 2,
+		PrevLogTerm:  2,
+		Entries: []LogEntry{
+			{Term: 2, Index: 2, Command: []byte("b")},
+		},
+		LeaderCommit: 1,
+	}
+
+	resp = node.AppendEntries(req2)
+	if !resp.Success {
+		t.Errorf("Expected failure")
+	}
+
+}
+
+func TestAppendEntriesWithConflict2(t *testing.T) {
+	/*
+		A node has recoverd with a log that is longer than the leaders
+		logs b and c should be deleted
+	*/
+
+	node := NewNode(1)
+	logger := &InMemoryLogger{
+		entries: []LogEntry{
+			{Term: 1, Index: 1, Command: []byte("init")},
+			{Term: 2, Index: 2, Command: []byte("a")},
+			{Term: 2, Index: 3, Command: []byte("b")},
+			{Term: 2, Index: 4, Command: []byte("c")},
+		},
+	}
+	node.state.logger = logger
+
+	req := AppendEntriesRequest{
+		Term:         3,
+		LeaderId:     123,
+		PrevLogIndex: 1,
+		PrevLogTerm:  1,
+		Entries: []LogEntry{
+			{Term: 3, Index: 2, Command: []byte("x")},
+			{Term: 3, Index: 3, Command: []byte("y")},
+			{Term: 3, Index: 4, Command: []byte("z")},
+		},
+		LeaderCommit: 1,
+	}
+
+	resp := node.AppendEntries(req)
+	if !resp.Success {
+		t.Errorf("Expected success")
+	}
+
+	log, _ := node.state.logger.Get(2)
+	if log.Term != 3 || log.Command[0] != 'x' {
+		t.Errorf("Wrong term %d %s", log.Term, string(log.Command[0]))
+	}
+}
