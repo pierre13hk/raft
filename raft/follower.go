405a68eb0bfe0c1327d83738f5dff53c81d68830
Pierre Van Remoortere <pierrevanremoortere@Pierres-MacBook-Pro.local>
2024-07-19 17:01:25 +0200

    fix: recvAppendEntries

--- raft/follower.go
+++ raft/follower.go
@@ -15,7 +15,7 @@ func (n *Node) recvAppendEntries(req AppendEntriesRequest) AppendEntriesResponse
 		n.role = Follower
 		n.state.currentTerm = req.Term
 		n.state.votedFor = req.LeaderId
-		close(n.electionChannel)
+		n.RestartHeartbeatTimer()
 		return AppendEntriesResponse{Term: n.state.currentTerm, Success: true}
 	}
 
@@ -26,49 +26,19 @@ func (n *Node) recvAppendEntries(req AppendEntriesRequest) AppendEntriesResponse
 		return AppendEntriesResponse{Term: n.state.currentTerm, Success: false}
 	}
 
-	if n.role == Candidate {
-		close(n.electionChannel)
-	}
 	n.role = Follower
 	n.state.votedFor = req.LeaderId
 	if len(req.Entries) == 0 {
-		// heartbeat
-		n.timerBackoffCounter = 1
+		n.RestartHeartbeatTimer()
 		log.Printf("Node %d: AppendEntries: Heartbeat from leader %d\n", n.state.id, req.LeaderId)
 		return AppendEntriesResponse{Term: n.state.currentTerm, Success: true}
 	}
 
-	//logs, _ := n.state.GetRange(req.PrevLogIndex + 1)
-	if req.Entries[0].Index <= n.state.LastLogIndex()-1 {
-		for _, entry := range req.Entries {
-			if entry.Index > n.state.LastLogIndex() {
-				break
-			}
-			localLog, err := n.state.Get(entry.Index)
-			if err != nil {
-				log.Println("shouldnt happen")
-				break
-			}
-			// A node can't have a more up to date log than the leader
-			// So req.Entries[i] should always be valid / in bounds
-			if entry.Term != localLog.Term {
-				log.Println("Node % d AppendEntries: Log doesn't match, truncating from ", n.state.id, entry.Index, entry.Command)
-				n.state.TruncateTo(entry.Index)
-				break
-			}
-		}
-	}
-
-	new := 0
-	for i, entry := range req.Entries {
-		_, err := n.state.Get(entry.Index)
-		if err != nil {
-			new = i
-			break
-		}
-	}
-	log.Printf("Node %d: AppendEntries: Appending %d new entries new = %d, len reqent %d\n", n.state.id, len(req.Entries[new:]), new, len(req.Entries))
-	n.state.Append(req.Entries[new:])
+	// Append new entries
+	n.state.TruncateTo(req.PrevLogIndex + 1)
+	n.state.Append(req.Entries)
+	last_appended_index := req.Entries[len(req.Entries)-1].Index
+	log.Printf("Node %d: AppendEntries: Appending %d new entries, last appended index= %d\n", n.state.id, len(req.Entries), last_appended_index)
 
 	return AppendEntriesResponse{n.state.currentTerm, true}
 }
