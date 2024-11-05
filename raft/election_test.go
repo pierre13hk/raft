11642b19d7877993f2aa0fcd14c72f9f93520eb0
Pierre Van Remoortere <pierrevanremoortere@Pierres-MacBook-Pro-2.local>
2024-11-05 20:45:22 +0100

    chore: ballot handling tests

--- raft/election_test.go
+++ raft/election_test.go
@@ -5,23 +5,20 @@ import (
 	//"time"
 )
 
-func TestRequestVote(t *testing.T) {
-	node1 := NewNode(1, "localhost:1234", NewInMemoryRaftRPC(), &DebugStateMachine{}, t.TempDir(), NodeConfig{})
-	node2 := NewNode(2, "localhost:1235", NewInMemoryRaftRPC(), &DebugStateMachine{}, t.TempDir(), NodeConfig{})
-	node3 := NewNode(3, "localhost:1236", NewInMemoryRaftRPC(), &DebugStateMachine{}, t.TempDir(), NodeConfig{})
-
-	node1.Peers = []Peer{{Id: 2}, {Id: 3}}
-
-	rpc := NewInMemoryRaftRPC()
-	rpc.Peers = map[uint64]*Node{
-		1: node1,
-		2: node2,
-		3: node3,
+var (
+	defaultNodeConfig = NodeConfig{
+		HeartbeatTimeout:   100,
+		ElectionTimeoutMin: 300,
+		ElectionTimeoutMax: 500,
 	}
+)
 
-	node1.RaftRPC = rpc
-	node2.RaftRPC = rpc
-	node3.RaftRPC = rpc
+func testNode() *Node {
+	return NewNode(1, "localhost:1234", NewInMemoryRaftRPC(), &DebugStateMachine{}, "/tmp", defaultNodeConfig)
+}
+
+func TestCheckVoteRequestGranted(t *testing.T) {
+	node := testNode()
 
 	b := Ballot{
 		Term:         0,
@@ -30,7 +27,107 @@ func TestRequestVote(t *testing.T) {
 		LastLogTerm:  0,
 	}
 
-	resp := node1.checkVoteRequest(b)
+	resp := node.checkVoteRequest(b)
+	if resp.VoteGranted {
+		t.Errorf("Expected vote not granted")
+	}
+}
+
+func TestCheckVoteRequestBallotLate(t *testing.T) {
+	// Test that we don't grant a vote if our term is greater
+	node := testNode()
+	node.state.currentTerm = 2
+	b := Ballot{
+		Term:         1,
+		CandidateId:  2,
+		LastLogIndex: 0,
+		LastLogTerm:  0,
+	}
+	resp := node.checkVoteRequest(b)
+	if resp.VoteGranted {
+		t.Errorf("Expected vote not granted")
+	}
+}
+
+func TestCheckVoteRequestAlreadyVoted(t *testing.T) {
+	// Test that we don't grant a vote if we already voted
+	node := testNode()
+	node.state.currentTerm = 1
+	node.state.votedFor = 3
+	b := Ballot{
+		Term:         2,
+		CandidateId:  2,
+		LastLogIndex: 0,
+		LastLogTerm:  0,
+	}
+	resp := node.checkVoteRequest(b)
+	if resp.VoteGranted {
+		t.Errorf("Expected vote not granted")
+	}
+}
+
+func TestCheckVoteRequestLogTermLate(t *testing.T) {
+	// Test that we don't grant a vote if the candidate's log's term is lesser
+	var CURRENT_TERM uint64 = 3
+	var CANDINDATE_TERM uint64 = 2
+	node := testNode()
+	node.state.currentTerm = CURRENT_TERM
+	node.state.votedFor = 0
+	entries := []LogEntry{
+		{
+			Term:    1,
+			Index:   1,
+			Type:    RAFT_LOG,
+			Command: []byte("test"),
+		},
+		{
+			Term:    CURRENT_TERM,
+			Index:   2,
+			Type:    USER_LOG,
+			Command: []byte("user command"),
+		},
+	}
+	node.state.Logger.Append(entries)
+
+	b := Ballot{
+		Term:         CANDINDATE_TERM,
+		CandidateId:  2,
+		LastLogIndex: 2,
+		LastLogTerm:  2,
+	}
+	resp := node.checkVoteRequest(b)
+	if resp.VoteGranted {
+		t.Errorf("Expected vote not granted")
+	}
+}
+
+func TestCheckVoteRequestLogIndexLate(t *testing.T) {
+	// Test that we don't grant a vote if the candidate's log's index is lesser
+	node := testNode()
+	node.state.currentTerm = 2
+	node.state.votedFor = 0
+	entries := []LogEntry{
+		{
+			Term:    1,
+			Index:   1,
+			Type:    RAFT_LOG,
+			Command: []byte("test"),
+		},
+		{
+			Term:    2,
+			Index:   2,
+			Type:    USER_LOG,
+			Command: []byte("user command"),
+		},
+	}
+	node.state.Logger.Append(entries)
+	b := Ballot{
+		Term:         2,
+		CandidateId:  2,
+		LastLogIndex: 1,
+		LastLogTerm:  2,
+	}
+	resp := node.checkVoteRequest(b)
 	if resp.VoteGranted {
 		t.Errorf("Expected vote not granted")
 	}
