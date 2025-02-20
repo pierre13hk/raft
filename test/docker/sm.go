e95e1741490354cebde38b8dbcb5746b10bb0e6f
Pierre Van Remoortere <pierre.van.remoortere@gmail.com>
2025-02-20 21:46:16 +0100

    feat(test): debug map state machine

---
+++ test/docker/sm.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type MapSM struct {
+	_map map[string]string
+}
+
+func (m *MapSM) splitCommand(command []byte) ([]string, error) {
+	parts := strings.Split(string(command), "|")
+	if len(parts) != 3 {
+		return nil, errors.New("error splitting command")
+	}
+	return parts, nil
+}
+
+func (m *MapSM) Apply(command []byte) error {
+	fmt.Println("MapSM Applying command", string(command))
+	parts, err := m.splitCommand(command)
+	if err != nil {
+		return err
+	}
+	cmd := parts[0]
+	key := parts[1]
+	value := parts[2]
+	switch cmd {
+	case "set":
+		m._map[key] = value
+	case "delete":
+		delete(m._map, key)
+	default:
+		return errors.New("unknown command")
+	}
+	return nil
+}
+
+func (m *MapSM) Read(command []byte) ([]byte, error) {
+	parts, err := m.splitCommand(command)
+	if err != nil {
+		return nil, err
+	}
+	key := parts[1]
+	value, ok := m._map[key]
+	if !ok {
+		return nil, errors.New("key not found")
+	}
+	return []byte(value), nil
+}
+
+func (m *MapSM) Serialize() ([]byte, error) {
+	buffer := new(bytes.Buffer)
+	encoder := gob.NewEncoder(buffer)
+	err := encoder.Encode(m._map)
+	if err != nil {
+		return nil, err
+	}
+	return buffer.Bytes(), nil
+}
+
+func (m *MapSM) Deserialize(data []byte) error {
+	buffer := bytes.NewBuffer(data)
+	decoder := gob.NewDecoder(buffer)
+	err := decoder.Decode(&m._map)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewMapSM() *MapSM {
+	return &MapSM{
+		_map: make(map[string]string),
+	}
+}
